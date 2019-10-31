20141ab66b2f9c52ef5c4517d6bf21d4c6a46ea2
Vutyan <viktor-timohov@mail.ru>
2019-10-31 12:30:25 +0300

    Add files via upload

--- hw_6.hs
+++ hw_6.hs
@@ -13,5 +13,38 @@ safeGet n list = case safePop list of
 
 
 -- Type Classes
--- Task 1: 
+-- Task 1: Class Reversible with Reverse Method
+class Reversible a where
+    rev :: a -> a
+
+instance Reversible Int where
+    rev = list2num.num2list where
+        num2list 0 = []
+        num2list n = (n `mod` 10): num2list (n `div` 10)
+        list2num = foldl (\a x -> 10 * a + x) 0
+
+instance Reversible [a] where
+    rev = foldl (\l x -> x:l) []
+
+-- Task 2: Reversible, Show, Eq and Ord Classes for Point2D 
+data Point2D = Point2D Int Int
+
+len :: Point2D -> Double
+len (Point2D x y) = sqrt $ fromIntegral (x * x + y * y)
+
+instance Reversible Point2D where
+    rev (Point2D a b) = Point2D (rev b) (rev a)
+
+instance Show Point2D where
+    show (Point2D a b) = "(" ++ show a ++ "; " ++ show b ++ ")"
+
+instance Eq Point2D where
+    (Point2D a1 b1) == (Point2D a2 b2) = (a1 == a2) && (b1 == b2)
+
+instance Ord Point2D where
+    compare p1 p2 | (len p1) == (len p2) = EQ
+                  | (len p1) > (len p2) = GT
+                  | (len p1) < (len p2) = LT
+
+
                             
